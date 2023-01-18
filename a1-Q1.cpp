adfe61d740a476e84929f18845d64c54c2d45101
saniyaverma <122946273+saniyaverma@users.noreply.github.com>
2023-01-18 19:25:09 +0530

    Create a1-Q1.cpp
    Assignment 1 | Question 1

---
+++ a1-Q1.cpp
@@ -0,0 +1,14 @@
+// to print z of size 5 using *
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"*****"<<endl;
+    cout<<"   *"<<endl;
+    cout<<"  *"<<endl;
+    cout<<" *"<<endl;
+    cout<<"******";
+
+    return 0;
+}
