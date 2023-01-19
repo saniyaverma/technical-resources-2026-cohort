b813a856ed76b3a208d743b30317f731d16f375a
saniyaverma <122946273+saniyaverma@users.noreply.github.com>
2023-01-20 01:42:09 +0530

    Create a2-Q12.cpp
    Assignment 2 | pattern 12

---
+++ a2-Q12.cpp
@@ -0,0 +1,23 @@
+// pattern 12
+
+#include<iostream>
+using namespace std;
+int main()
+{
+   int i,j,num,f0=0,f1=1,f2=1;
+   cout<<"Enter a number : ";
+   cin>>num;
+   cout<<f0<<endl<<f1<<" "<<f2<<endl;
+   for(i=3;i<=num;i++)
+   {
+       for(j=1;j<=i;j++)
+       {
+          f0=f1;
+          f1=f2;
+          f2=f1+f0;
+          cout<<f2<<" ";
+       }
+       cout<<endl;
+   }
+   return 0;
+}
