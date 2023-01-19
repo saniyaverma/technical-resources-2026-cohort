521d082804e6947baf9977afd96cd0b0ed398e3e
saniyaverma <122946273+saniyaverma@users.noreply.github.com>
2023-01-20 01:55:39 +0530

    Update a2-Q12.cpp
    Assignment 2 | pattern 12

--- a2-Q12.cpp
+++ a2-Q12.cpp
@@ -8,7 +8,7 @@ int main()
    cout<<"Enter a number : ";
    cin>>num;
    cout<<f0<<endl<<f1<<" "<<f2<<endl;
-   for(i=3;i<=num;i++)
+   for(i=3;i<=num+3;i++)
    {
        for(j=1;j<=i;j++)
        {
