4880d9fef1e45947ff0f93eaf587f9f4486fecc5
saniyaverma <122946273+saniyaverma@users.noreply.github.com>
2023-01-19 23:12:20 +0530

    Update a2-Q7.cpp
    Assignment 2 | pattern 7

--- a2-Q7.cpp
+++ a2-Q7.cpp
@@ -5,16 +5,16 @@
 using namespace std;
 int main()
 {
-    int i,j,n;
+    int num;
     cout<<"Enter a number : ";
-    cin>>n;
-    for(i=0;i<n;i++)
+    cin>>num;
+    for(int i=1;i<=num;i++)
     {
-        for(j=0;j>=i;j++)
+        for(int j=1;j<=i;j++)
         {
-            if(i=j)
+            if(i==j)
             cout<<"*";
-            else
+            if(i>j)
             cout<<" ";
         }
     cout<<endl;
