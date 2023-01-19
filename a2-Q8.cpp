9886ff19f25dcf4b14d19c8ff5d3ef825ee0a4e8
saniyaverma <122946273+saniyaverma@users.noreply.github.com>
2023-01-19 23:16:33 +0530

    Create a2-Q8.cpp
    Assignment 2 | pattern 8

---
+++ a2-Q8.cpp
@@ -0,0 +1,23 @@
+// pattern 8 
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a number : ";
+    cin>>num;
+    for(int i=1;i<=num;i++)
+    {
+        for(int j=1;j<=num;j++)
+        {
+            if((i+j)==num+1)
+            cout<<"*";
+            else
+            cout<<" ";
+        }
+    cout<<endl;
+    }
+    return 0;
+}
